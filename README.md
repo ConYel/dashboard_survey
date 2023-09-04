7e1fb98a35c09847a094f34954ec19e0822e55b0
ConYel <yeles.konstantinos@gmail.com>
2023-09-04 09:38:38 +0200

    Shiny update and testing dataset

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # dashboard_survey
+
+for testing run:
+```r
+Rscript dashboard_survey.R testdt.txt
+```
---
+++ dashboard_survey.R
@@ -0,0 +1,164 @@
+args <- commandArgs(TRUE)
+print(paste0("file to be used: ", args[1]))
+print("Loading libraries")
+## libraries
+my_packages <- c("vroom", "shiny", "shinydashboard", "DT",
+                 "purrr",
+                 "dplyr", "stringr", "ggplot2", "plotly")
+suppressPackageStartupMessages(
+    invisible(lapply(my_packages, library, character.only = TRUE)))
+#library(sf)
+#library(spData)
+print("--- Libraries are loaded ---")
+# read the dataset
+main_dataset <- vroom(args[1], show_col_types = FALSE)
+
+
+print("Dataset was imported")
+#print(head(main_dataset))
+
+
+PhD_theme <-
+    list(
+        #scale_fill_manual(values = wes_cols),
+        #scale_color_manual(values = wes_cols),
+        theme_bw() +
+            theme(
+                panel.border = element_blank(),
+                strip.background = element_blank(),
+                strip.text = element_text(size = 12, colour = "black"),
+                axis.line = element_line(),
+                panel.grid.major = element_line(linewidth = 0.2),
+                panel.grid.minor = element_line(linewidth = 0.1),
+                text = element_text(size = 12),
+                axis.title.x = element_text(margin = margin(t = 10, r = 10, b = 10, l = 10),
+                                            colour = "black"),
+                axis.title.y = element_text(margin = margin(t = 10, r = 10, b = 10, l = 10),
+                                            colour = "black"),
+                axis.text.x = element_text(hjust = 1, vjust = 1, size = 12),
+                axis.text.y = element_text(size = 12, colour = "black"),
+                plot.title = element_text(hjust = 0.5, colour = "black")
+            )
+    )
+
+print("Calculate how many countries per plot")
+num_groups <- main_dataset$country %>% unique() %>% length() %/%20
+remai <- main_dataset$country %>% unique() %>% length() %%20
+if (remai > 0) { num_groups <- num_groups + 1}
+
+country_codes <- main_dataset %>%
+    group_by(country) %>%
+    group_keys() %>%
+    group_by((row_number()-1) %/% (n()/num_groups)) %>%
+    tidyr::nest() %>%
+    pull(data) %>%
+    bind_rows(.id = "country_table")
+
+main_dataset <- main_dataset %>%
+    left_join(country_codes,by = join_by(country) )
+
+print("Preparing Shiny Dashboard: plots")
+list_plots <- unique(main_dataset$country_table) %>%
+    str_sort() %>%
+    purrr::map(~main_dataset %>%
+                   mutate(country = str_trunc(country, 30)) %>%
+                   filter(country_table == .x) %>%
+                   ggplot(aes(country, fill = group))+
+                   geom_bar(position = "identity") +
+                   ggtitle(label = str_c("Responses from part:", .x, " countries"))+
+                   coord_flip()+
+                   PhD_theme
+    ) %>%
+    purrr::map(~ggplotly(p=.x))
+
+plotUI <- function(id) {
+    ns <- NS(id)
+
+    plotlyOutput(ns("plot"))
+}
+
+plotServer <- function(id, table_number) {
+    moduleServer(
+        id,
+        function(input, output, session) {
+            output$plot <- renderPlotly({
+                req(table_number)
+                list_plots[[table_number]]
+            })
+        }
+    )
+}
+
+print("Preparing Shiny Dashboard: sidebar")
+
+sidebar <- dashboardSidebar(
+    sidebarMenu(
+        menuItem(
+            text = "General Statistics",
+            tabName = "gen_stat",
+            icon = icon("earth-europe")),
+        menuItem(
+            text = "Table",
+            tabName = "dt_stat",
+            icon = icon("dashboard"))
+    )
+)
+
+print("Preparing Shiny Dashboard: body")
+body <- dashboardBody(
+    tabItems(
+        tabItem(tabName = "gen_stat",
+                h2("General dataset barplots"),
+                fluidRow(
+                    uiOutput("plotUI")
+                    )),
+        tabItem(tabName = "dt_stat",
+                h2("Dataset Table"),
+                fluidRow(
+                    DT::dataTableOutput("rawtable"))
+        )
+    )
+)
+
+print("Preparing Shiny Dashboard: UI")
+ui <- dashboardPage(
+    dashboardHeader(title = "REMO Survey"),
+    sidebar = sidebar,
+    body = body
+)
+
+
+print("Preparing Shiny Dashboard: Server")
+server <- function(input, output, session) {
+    # end the session when browser is closed
+    # will be removed if it is on server
+    session$onSessionEnded(function() {
+        stopApp()
+    })
+
+    output$plotUI <- renderUI({
+        ns <- session$ns
+        tagList(
+            lapply(1:num_groups,
+                   function(i) {
+                       plotUI(paste0("plot", i))
+                   }
+            )
+        )
+    })
+
+#    observeEvent(c(input$x, input$y), {
+        plotServerList <- lapply(
+            1:num_groups,
+            function(i) {
+                plotServer(paste0("plot", i), i)
+            }
+        )
+#    })
+
+    output$rawtable <- DT::renderDataTable((DT::datatable(main_dataset)))
+}
+
+print("Deploying Dashboard")
+shinyApp(ui, server, options = list(launch.browser = TRUE))
+print("Finished.")
